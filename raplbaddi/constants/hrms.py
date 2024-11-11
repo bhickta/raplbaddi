3901e13437fee92b9554e6ba5d375da9b38e95bf
installer <installer@gmail.com>
2024-11-11 17:42:09 +0000

    feat: depends on null in bank details employee, p2p

--- raplbaddi/constants/hrms.py
+++ raplbaddi/constants/hrms.py
@@ -42,8 +42,31 @@ custom_fields = {
             "hidden": 0,
             "no_copy": 0,
         },
-    ]
+    ],
 }
+
 
 def execute():
     create_custom_fields(custom_fields)
+
+
+def get_property_setters():
+    property_setters = []
+    for field in ["bank_name", "bank_ac_no", "iban"]:
+        property_setters.append(
+                {
+            "name": f"Employee-{field}-depends_on",
+            "owner": "Administrator",
+            "docstatus": 0,
+            "idx": 0,
+            "is_system_generated": 1,
+            "doctype_or_field": "DocField",
+            "doc_type": "Employee",
+            "field_name": f"{field}",
+            "property": "depends_on",
+            "property_type": "Code",
+            "value": "",
+            "doctype": "Property Setter",
+        })
+
+    return property_setters
