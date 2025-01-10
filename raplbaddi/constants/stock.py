4592172a73756ac25b797eb1e9a50013a9333184
installer <installer@gmail.com>
2025-01-10 01:53:10 +0000

    feat: cbm in items prod migrate restart

--- raplbaddi/constants/stock.py
+++ raplbaddi/constants/stock.py
@@ -3,6 +3,56 @@ from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 custom_fields = {
     "Item": [
         {
+            "is_system_generated": 1,
+            "label": "CBM Section MM",
+            "fieldname": "cbm_section",
+            "insert_after": "uoms",
+            "fieldtype": "Section Break",
+            "collapsible": 1,
+        },
+        {
+            "name": "Item-cbm_length",
+            "label": "CBM Length",
+            "fieldname": "cbm_length",
+            "insert_after": "cbm_section",
+            "fieldtype": "Float",
+        },
+        {
+            "is_system_generated": 1,
+            "fieldname": "cbm_cb_1",
+            "insert_after": "cbm_length",
+            "fieldtype": "Column Break",
+        },
+        {
+            "name": "Item-cbm_width",
+            "label": "CBM Width",
+            "fieldname": "cbm_width",
+            "insert_after": "cbm_cb_1",
+            "fieldtype": "Float",
+        },
+        {
+            "name": "Item-cbm_height",
+            "label": "CBM Height",
+            "fieldname": "cbm_height",
+            "insert_after": "cbm_width",
+            "fieldtype": "Float",
+        },
+        {
+            "is_system_generated": 1,
+            "fieldname": "cbm_section_1",
+            "insert_after": "cbm_height",
+            "fieldtype": "Section Break",
+            "collapsible": 0,
+        },
+        {
+            "name": "Item-cbm",
+            "label": "CBM",
+            "fieldname": "cbm",
+            "insert_after": "cbm_height",
+            "fieldtype": "Float",
+            "read_only": 1,
+        },
+        {
             "name": "Item-unit",
             "label": "Unit",
             "fieldname": "unit",
--- raplbaddi/overrides/item.py
+++ raplbaddi/overrides/item.py
@@ -3,6 +3,11 @@ from erpnext.stock.doctype.item.item import get_uom_conv_factor
 
 def validate(doc, method):
     validate_uom_conversion_factor(doc)
+    validate_cbm(doc)
+
+def validate_cbm(doc):
+    if doc.cbm_height and doc.cbm_width and doc.cbm_length:
+        doc.cbm = (doc.cbm_height * doc.cbm_width * doc.cbm_length)/1000000000
 
 def validate_uom_conversion_factor(doc):
     def validate_gram_kgs_conversion_multiple(doc):
