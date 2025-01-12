e59c77900f9e16b6387beb4d1cfda35817411316
installer <installer@gmail.com>
2025-01-12 16:06:35 +0000

    patch: distance january issuesrapl prod migrate restart

--- raplbaddi/patches/set_distance_issuerapl.py
+++ raplbaddi/patches/set_distance_issuerapl.py
@@ -5,8 +5,10 @@ def execute():
     issuerapl_docs = frappe.get_all(
         "IssueRapl", {
             "docstatus": 0,
-            "creation": [">=", "2025-01-01"]
-        }, pluck="name")
+            "creation": [">=", "2025-01-01"],
+            "aerial_kilometer": ["is", "set"]
+        }, pluck="name")[0:1]
+    print(issuerapl_docs)
     for doc in issuerapl_docs:
         doc = frappe.get_doc("IssueRapl", doc)
         set_kilometers(doc)
