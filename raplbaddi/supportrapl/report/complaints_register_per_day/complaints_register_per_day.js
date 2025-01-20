bb284932841940af4bb971542c9479dbd22be55b
installer <installer@gmail.com>
2025-01-20 15:55:57 +0000

    clean: issuerapl service centre reports

--- raplbaddi/supportrapl/report/complaints_register_per_day/complaints_register_per_day.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, Nishant Bhickta and contributors
-// For license information, please see license.txt
-
-frappe.query_reports["Complaints Register Per Day"] = {
-	"filters": [
-
-	]
-};
