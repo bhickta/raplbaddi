d10ee0ef97f38b25fe4ae4b185af2fc2f8dea975
bhickta <nishantbhickta+code@gmail.com>
2025-02-03 09:35:06 +0000

    sitcker source

--- raplbaddi/raplbaddi/doctype/sticker_type/sticker_type.py
+++ raplbaddi/raplbaddi/doctype/sticker_type/sticker_type.py
@@ -1,11 +1,11 @@
-# Copyright (c) 2024, Nishant Bhickta and contributors
+# Copyright (c) 2025, Nishant Bhickta and contributors
 # For license information, please see license.txt
 
 # import frappe
 from frappe.model.document import Document
 
 
-class Sticker(Document):
+class StickerType(Document):
 	# begin: auto-generated types
 	# This code is auto-generated. Do not modify anything in this block.
 
@@ -14,10 +14,6 @@ class Sticker(Document):
 	if TYPE_CHECKING:
 		from frappe.types import DF
 
-		nature: DF.Literal["Common", "Not Common"]
-		sample: DF.AttachImage | None
-		source: DF.Link
-		sticker_type: DF.Link
-		title: DF.Data
+		title: DF.Data | None
 	# end: auto-generated types
 	pass
---
+++ raplbaddi/raplbaddi/doctype/sticker_type/test_sticker_type.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Nishant Bhickta and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestStickerType(FrappeTestCase):
+	pass
