7f1da682d36375e984818301772091af4ff78ee2
bhickta <61617746+bhickta@users.noreply.github.com>
2025-03-08 05:29:02 +0000

    patch of sc stock recon, dn and pr

--- raplbaddi/patches/sc_stock_recon.py
+++ raplbaddi/patches/sc_stock_recon.py
@@ -43,7 +43,7 @@ class StockReconciliationManager:
             if other_items:
                 self.create_new_reconciliation(stock_reconciliation, other_items, stock_reconciliation.posting_date)
         except Exception as e:
-            print(f"Error processing {stock_reconciliation_id}: {str(e)}")
+            raise e
 
     def create_amended_reconciliation(self, stock_reconciliation, items, posting_date):
         changed_fields = {"posting_date": posting_date, "set_posting_time": True}
@@ -51,7 +51,7 @@ class StockReconciliationManager:
         amended_stock_reconciliation = self.amend_document(stock_reconciliation, changed_fields, rows_updated, submit=True)
         main_warehouse = items[0].warehouse
         sc_details = self.set_service_centre_details({"main_warehouse": main_warehouse})
-        self.change_warehouse_in_dnpr(sc_details, posting_date)
+        self.change_warehouse_in_dnpr(sc_details, main_warehouse, posting_date)
         print(f"Successfully created amended Stock Reconciliation {amended_stock_reconciliation.name} with date {posting_date}")
 
     def create_new_reconciliation(self, stock_reconciliation, items, posting_date):
@@ -109,26 +109,51 @@ class StockReconciliationManager:
     def set_service_centre_details(self, filters=None):
         return frappe.get_all("Service Centre", fields=["name", "main_warehouse", "customer", "supplier"], filters=filters)[0]
 
-    def change_warehouse_in_dnpr(self, sc, posting_date):
+    def process_documents(self, doctype, party_field, party_name, new_warehouse, error_log):
+        doc_list = frappe.get_all(
+            doctype,
+            filters={
+                party_field: party_name,
+                "posting_date": ["between", [self.start_date, self.end_date]],
+                "docstatus": 1,
+            },
+            pluck="name"
+        )
+        
+        for doc_name in doc_list:
+            doc = frappe.get_doc(doctype, doc_name)
+            doc.cancel()
+            
+            for item in doc.items:
+                item.warehouse = new_warehouse
+            
+            new_doc = self.amend_document(
+                doc,
+                changed_fields={"posting_date": doc.posting_date, "set_posting_time": True},
+                rows_updated={"items": doc.items},
+                submit=False,
+            )
+            
+            try:
+                new_doc.submit()
+            except erpnext.exceptions.PartyDisabled:
+                frappe.db.set_value(party_field.split("_")[0].capitalize(), party_name, "disabled", 0)
+                new_doc.submit()
+                frappe.db.set_value(party_field.split("_")[0].capitalize(), party_name, "disabled", 1)
+            except FileNotFoundError as e:
+                new_doc.submit()
+            except Exception as e:
+                frappe.log_error(frappe.get_traceback(), error_log)
+                raise e
+            
+            print(f"Successfully processed {new_doc}")
+
+    def change_warehouse_in_dnpr(self, sc, new_warehouse, posting_date):
         self.start_date = posting_date
         self.end_date = frappe.utils.today()
-        dnpr = frappe.get_all("Delivery Note", fields=["name"], filters={"customer": sc.customer, "posting_date": ["between", [self.start_date, self.end_date]], "docstatus": 1})
-        for dn in dnpr:
-            dn = frappe.get_doc("Delivery Note", dn.name)
-            dn.cancel()
-            changed_fields = {"posting_date": dn.posting_date, "set_posting_time": True}
-            rows_updated = {"items": dn.items}
-            new_dn = self.amend_document(dn, changed_fields, rows_updated, submit=True)
-            print(new_dn)
-            try:
-                new_dn.submit()
-            except erpnext.exceptions.PartyDisabled as e:
-                frappe.db.sql(f"UPDATE `tabCustomer` SET disabled = 0 WHERE name = '{sc.customer}'")
-                new_dn.submit()
-                frappe.db.sql(f"UPDATE `tabCustomer` SET disabled = 1 WHERE name = '{sc.customer}'")
-
-            except Exception as e:
-                raise e
+        
+        self.process_documents("Delivery Note", "customer", sc.customer, new_warehouse, "DN Submit Error")
+        self.process_documents("Purchase Receipt", "supplier", sc.supplier, new_warehouse, "PR Submit Error")
 
     @staticmethod
     def execute(file_name):
@@ -137,3 +162,4 @@ class StockReconciliationManager:
     
 def execute():
     StockReconciliationManager.execute("stock_reconcilliation_sc.csv")
+    frappe.db.commit()
