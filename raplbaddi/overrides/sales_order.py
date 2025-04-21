f757300e7050b808c866d893f95f1bfd8d5ff610
bhickta <61617746+bhickta@users.noreply.github.com>
2025-04-21 03:46:07 +0000

    fix: custom box in sales order to backend and logic corretion refactor

--- raplbaddi/overrides/sales_order.py
+++ raplbaddi/overrides/sales_order.py
@@ -1,4 +1,5 @@
 import frappe
+import frappe.utils
 
 def validate(doc, method):
     validate_bill_to_ship_to(doc)
@@ -20,3 +21,32 @@ def validate_bill_to_ship_to(doc):
     shipping_gstin = frappe.get_value("Address", doc.shipping_address_name, "gstin")
     if billing_gstin != shipping_gstin and not bill_to_ship_to:
         frappe.throw(("Please Check Bill To and Ship To if Billing and Shipping GSTIN are not same"))
+
+@frappe.whitelist()
+def update_custom_box_backend(item_code, name_of_brand, cdt, cdn):
+    geyser_details = frappe.db.get_value('Item', item_code, 'geyser_box_size', as_dict=True)
+    if not geyser_details or not geyser_details.get('geyser_box_size'):
+        return {'custom_box': ''}
+
+    box_size_details = frappe.db.get_value('Geyser Box Size', geyser_details['geyser_box_size'], ['capacity', 'model'], as_dict=True)
+    if not box_size_details:
+        return {'custom_box': ''}
+
+    packing_boxes = frappe.db.get_list(
+        'Item',
+        fields=['name'],
+        filters={
+            'item_group': 'Packing Boxes',
+            'capacity': box_size_details['capacity'],
+            'geyser_model': box_size_details['model'],
+            'brand': name_of_brand,
+            'disabled': 0,
+        }
+    )
+    if len(packing_boxes) > 1:
+        error = [f"Multiple packing boxes found for {item_code} with capacity {box_size_details['capacity']} and model {box_size_details['model']}. Please check the packing box item group."]
+        error.append(f"Packing Boxes: {', '.join([frappe.utils.get_link_to_form('Item', box['name']) for box in packing_boxes])}")
+        frappe.throw("<br>".join(error))
+
+    custom_box = packing_boxes[0]['name']
+    return {'custom_box': custom_box}
--- raplbaddi/public/js/sales_order.js
+++ raplbaddi/public/js/sales_order.js
@@ -1,5 +1,33 @@
-frappe.ui.form.on("Sales Order", {
-  refresh: function (frm) {
+let deliveredToggleCount = 1;
+
+frappe.ui.form.on('Sales Order', {
+  onload(frm) {
+    if (frm.doc.docstatus === 1) {
+      frm.add_custom_button(__('Toggle Delivered'), () => {
+        frm.doc.items.forEach((row, idx) => {
+          if (row.qty <= row.delivered_qty) {
+            const itemRow = $(`[data-idx='${idx + 1}']`);
+            deliveredToggleCount % 2 ? itemRow.hide() : itemRow.show();
+          }
+        });
+        deliveredToggleCount++;
+      });
+    }
+
+    if (frm.doc.customer) {
+      setShippingAddressQuery(frm);
+    }
+
+    if (frm.doc.docstatus === 1 && frappe.user.name === 'kumarom906@gmail.com' && frm.doc.conditions) {
+      frappe.warn('These are the Remarks', frm.doc.conditions, () => { }, 'Continue', true);
+    }
+
+    if (!frm.doc.customer && frm.doc.delivery_date && frm.doc.no_delivery_date) {
+      frm.set_value('delivery_date', frappe.datetime.year_end());
+    }
+  },
+
+  refresh(frm) {
     frm.events.queries(frm);
   },
   queries(frm) {
@@ -12,4 +40,128 @@ frappe.ui.form.on("Sales Order", {
       };
     });
   },
+
+  before_save(frm) {
+    let zeroDiscountItems = [];
+
+    frm.doc.items.forEach(item => {
+      frappe.model.set_value(item.doctype, item.name, 'warehouse', item.name_of_brand + ' - RAPL');
+      frappe.model.set_value(item.doctype, item.name, 'delivery_date', frm.doc.delivery_date);
+      frappe.model.set_value(item.doctype, item.name, 'discount_amount', Math.round(item.discount_amount));
+      frappe.model.set_value(item.doctype, item.name, 'rate', Math.round(item.rate));
+
+      if (item.rate <= 0) {
+        frappe.throw(`Rate of ${item.item_code} should not be 0`);
+      }
+      if (item.brand === 'Red Star' && /K(WT?|BH)?$/.test(item.item_code)) {
+        frappe.throw('Red Star Brand cannot be selected in Kiya Models');
+      }
+      if (!item.custom_box) {
+        frappe.model.set_value(item.doctype, item.name, 'custom_box', 'Any Plain');
+      }
+      if (item.discount_amount <= 0) {
+        zeroDiscountItems.push(`${item.item_code} - ${item.name_of_brand}`);
+      }
+    });
+
+    if (zeroDiscountItems.length) {
+      frappe.warn('Possible Wrong Discounts', zeroDiscountItems.join('\n'));
+    }
+  },
+
+  after_save(frm) {
+    if (!frm.doc.shipping_address_name) {
+      frm.set_value('shipping_address_name', frm.doc.customer_address);
+      frm.refresh_field('shipping_address_name');
+    }
+  },
+
+  before_submit(frm) {
+    frm.set_value('submission_date', frappe.datetime.now_date());
+  },
+
+  before_cancel(frm) {
+    frm.events.ask_for_reason_of_cancellation(frm);
+  },
+  ask_for_reason_of_cancellation(frm) {
+    let dialog = new frappe.ui.Dialog({
+      title: __('Reason for Cancellation'),
+      fields: [{ fieldname: 'reason_for_cancellation', fieldtype: 'Text', reqd: 1 }],
+      primary_action: () => {
+        let data = dialog.get_values();
+        frappe.call({
+          method: 'frappe.desk.form.utils.add_comment',
+          args: {
+            reference_doctype: frm.doctype,
+            reference_name: frm.docname,
+            content: __('Reason for Cancellation:') + ' ' + data.reason_for_cancellation,
+            comment_email: frappe.session.user,
+            comment_by: frappe.session.user_fullname
+          },
+          callback(r) {
+            if (!r.exc) dialog.hide();
+          }
+        });
+      }
+    });
+    dialog.show();
+  }
 });
+
+frappe.ui.form.on('Sales Order Item', {
+  items_add(frm, cdt, cdn) {
+    copyPrevious(frm, locals[cdt][cdn], ['name_of_brand']);
+  },
+  qty(frm, cdt, cdn) {
+    copyPrevious(frm, locals[cdt][cdn], ['discount_amount']);
+  },
+  name_of_brand(frm, cdt, cdn) {
+    updateCustomBox(frm, cdt, cdn);
+  },
+  item_code(frm, cdt, cdn) {
+    updateCustomBox(frm, cdt, cdn);
+  },
+  plain_type(frm, cdt, cdn) {
+    let row = frm.get_field('items').grid.get_row(cdn).doc;
+    if (row.box_type === 'Printed') {
+      frappe.model.set_value(cdt, cdn, 'plain_type', '');
+      frappe.show_alert('Set Either - Printed Or Plain Type');
+    }
+  }
+});
+
+function copyPrevious(frm, row, fields) {
+  let items = frm.doc.items;
+  if (items.length > 1 && row.idx > 1) {
+    fields.forEach(field => {
+      let prev = items[row.idx - 2][field];
+      frappe.model.set_value(row.doctype, row.name, field, prev);
+    });
+  }
+}
+
+function updateCustomBox(frm, cdt, cdn) {
+  let row = locals[cdt][cdn];
+  if (!row.item_code || !row.name_of_brand) return;
+  frappe.call({
+    method: "raplbaddi.overrides.sales_order.update_custom_box_backend",
+    args: {
+      item_code: row.item_code,
+      name_of_brand: row.name_of_brand,
+      cdt: cdt,
+      cdn: cdn
+    },
+    callback: function(r) {
+      if (!r.exc) {
+        frappe.model.set_value(cdt, cdn, 'custom_box', r.message.custom_box);
+        frm.refresh_field('custom_box');
+      } else {
+        console.error("Error in backend function:", r.exc);
+      }
+    }
+  });
+}
+
+function setShippingAddressQuery(frm) {
+  frm.set_query('shipping_address_name', () => ({ filters: {} }));
+}
