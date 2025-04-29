c5b8b8acb901ff3a899ed9a1c101628ff2b8aea2
bhickta <61617746+bhickta@users.noreply.github.com>
2025-04-29 14:45:20 +0000

    fix: added multiple date ranges for the report

--- raplbaddi/raplbaddi/report/pb_analysis/pb_analysis.js
+++ raplbaddi/raplbaddi/report/pb_analysis/pb_analysis.js
@@ -1,28 +1,39 @@
-// Copyright (c) 2025, Frappe Technologies Pvt. Ltd. and contributors
-// For license information, please see license.txt
-
 frappe.query_reports["PB Analysis"] = {
-    "filters": [
+    filters: [
         {
-            "fieldname": "item_group",
-            "label": "Item Group",
-            "fieldtype": "Link",
-            "options": "Item Group",
-            "reqd": 1,
+            fieldname: "item_group",
+            label: __("Item Group"),
+            fieldtype: "Link",
+            options: "Item Group",
+            reqd: 1,
         },
         {
-            "fieldname": "from_date",
-            "label": "From Date",
-            "fieldtype": "Date",
-            "default": frappe.datetime.add_months(frappe.datetime.get_today(), -12),
-            "reqd": 0
+            fieldname: "from_date_1",
+            label: __("From Date 1"),
+            fieldtype: "Date",
+            default: frappe.datetime.year_start(),
+            reqd: 1,
         },
         {
-            "fieldname": "to_date",
-            "label": "To Date",
-            "fieldtype": "Date",
-            "default": frappe.datetime.get_today(),
-            "reqd": 0
+            fieldname: "to_date_1",
+            label: __("To Date 1"),
+            fieldtype: "Date",
+            default: frappe.datetime.year_end(),
+            reqd: 1,
         },
-    ]
-}
+        {
+            fieldname: "from_date_2",
+            label: __("From Date 2"),
+            fieldtype: "Date",
+            default: frappe.datetime.add_months(frappe.datetime.year_start(), -12),
+            reqd: 1,
+        },
+        {
+            fieldname: "to_date_2",
+            label: __("To Date 2"),
+            fieldtype: "Date",
+            default: frappe.datetime.add_months(frappe.datetime.year_end(), -12),
+            reqd: 1,
+        }
+    ],
+};
--- raplbaddi/raplbaddi/report/pb_analysis/pb_analysis.py
+++ raplbaddi/raplbaddi/report/pb_analysis/pb_analysis.py
@@ -1,4 +1,5 @@
 import frappe
+import re
 
 
 class StockMovementReport:
@@ -8,200 +9,106 @@ class StockMovementReport:
         self.data = []
 
     def get_columns(self):
-        return [
-            {
-                "label": "Item Code",
-                "fieldname": "item_code",
-                "fieldtype": "Link",
-                "options": "Item",
-                "width": 120,
-            },
-            {
-                "label": "Item Name",
-                "fieldname": "item_name",
-                "fieldtype": "Data",
-                "width": 150,
-            },
-            {
-                "label": "Stock Qty",
-                "fieldname": "stock_qty",
-                "fieldtype": "Float",
-                "width": 100,
-            },
-            {
-                "label": "DN Qty (Sales Range)",
-                "fieldname": "dn_qty",
-                "fieldtype": "Float",
-                "width": 150,
-            },
-            {
-                "label": "PR Qty (Purchase Range)",
-                "fieldname": "pr_qty",
-                "fieldtype": "Float",
-                "width": 150,
-            },
-            {
-                "label": "SO Qty (via DN)",
-                "fieldname": "so_qty",
-                "fieldtype": "Float",
-                "width": 150,
-            },
+        columns = [
+            {"label": "Item Code", "fieldname": "item_code", "fieldtype": "Link", "options": "Item", "width": 120},
+            {"label": "Item Name", "fieldname": "item_name", "fieldtype": "Data", "width": 150},
+            {"label": "Stock Qty Current", "fieldname": "stock_qty", "fieldtype": "Float", "width": 100},
         ]
+        for i in self.get_range_indices():
+            from_date = self.filters.get(f"from_date_{i}")
+            to_date = self.filters.get(f"to_date_{i}")
+            date_range_label = f"{from_date} to {to_date}"
+            columns.extend([
+                {"label": f"DN Qty ({date_range_label})", "fieldname": f"dn_qty_{i}", "fieldtype": "Float", "width": 180},
+                {"label": f"PR Qty ({date_range_label})", "fieldname": f"pr_qty_{i}", "fieldtype": "Float", "width": 180},
+                {"label": f"SO Qty ({date_range_label})", "fieldname": f"so_qty_{i}", "fieldtype": "Float", "width": 180},
+            ])
+        return columns
+
+    def get_range_indices(self):
+        pattern = re.compile(r"from_date_(\d+)")
+        return sorted(set(int(match.group(1)) for key in self.filters for match in [pattern.match(key)] if match))
 
     def fetch_items(self):
         conditions = []
         params = {}
-
         if self.filters.get("item_group"):
             conditions.append("i.item_group = %(item_group)s")
             params["item_group"] = self.filters.get("item_group")
-
-        where_clause = "WHERE " + " AND ".join(conditions) if conditions else ""
-
-        return frappe.db.sql(
-            f"""
-            SELECT
-                i.name AS item_code,
-                i.item_name
+        
+        where_clause = f"WHERE {' AND '.join(conditions)}" if conditions else ""
+        
+        return frappe.db.sql(f"""
+            SELECT i.name AS item_code, i.item_name
             FROM `tabItem` i
             {where_clause}
-        """,
-            params,
-            as_dict=1,
-        )
+        """, params, as_dict=1)
 
     def fetch_bin_data(self, item_codes):
         if not item_codes:
-            return []
-
-        bin_data = frappe.db.sql(
-            """
-            SELECT
-                bin.item_code,
-                Sum(bin.actual_qty) AS actual_qty
+            return {}
+        
+        rows = frappe.db.sql("""
+            SELECT bin.item_code, SUM(bin.actual_qty) AS actual_qty
             FROM `tabBin` bin
             WHERE bin.item_code IN %(item_codes)s
             GROUP BY bin.item_code
-        """,
-            {"item_codes": item_codes},
-            as_dict=1,
-        )
-        return bin_data
+        """, {"item_codes": item_codes}, as_dict=1)
+        
+        return {row.item_code: row.actual_qty for row in rows}
 
-    def fetch_dn_quantities(self, item_codes):
-        if not item_codes or not (
-            self.filters.get("from_date") and self.filters.get("to_date")
-        ):
-            return []
+    def fetch_qty_map(self, item_codes, doctype, child_table, item_field, date_field, from_date, to_date):
+        if not item_codes:
+            return {}
 
-        return frappe.db.sql(
-            """
-            SELECT
-                dni.item_code as item_code,
-                SUM(dni.qty) AS dn_qty
-            FROM `tabDelivery Note Item` dni
-            JOIN `tabDelivery Note` dn ON dn.name = dni.parent
-            WHERE
-                dni.item_code IN %(item_codes)s
-                AND dn.posting_date BETWEEN %(from_date)s AND %(to_date)s
-                AND dn.docstatus = 1
-            GROUP BY dni.item_code
-        """,
-            {
-                "item_codes": item_codes,
-                "from_date": self.filters["from_date"],
-                "to_date": self.filters["to_date"],
-            },
-            as_dict=1,
-        )
-
-    def fetch_pr_quantities(self, item_codes):
-        if not item_codes or not (
-            self.filters.get("from_date") and self.filters.get("to_date")
-        ):
-            return []
-
-        return frappe.db.sql(
-            """
-            SELECT
-                pri.item_code,
-                SUM(pri.qty) AS pr_qty
-            FROM `tabPurchase Receipt Item` pri
-            JOIN `tabPurchase Receipt` pr ON pr.name = pri.parent
-            WHERE
-                pri.item_code IN %(item_codes)s
-                AND pr.posting_date BETWEEN %(from_date)s AND %(to_date)s
-                AND pr.docstatus = 1
-            GROUP BY pri.item_code
-        """,
-            {
-                "item_codes": item_codes,
-                "from_date": self.filters["from_date"],
-                "to_date": self.filters["to_date"],
-            },
-            as_dict=1,
-        )
-
-    def fetch_so_quantities(self, item_codes):
-        if not item_codes or not (
-            self.filters.get("from_date") and self.filters.get("to_date")
-        ):
-            return []
-
-        so_data = frappe.db.sql(
-            """
-            SELECT
-            soi.custom_box AS item_code,
-            SUM(soi.qty) AS so_qty
-            FROM `tabSales Order Item` soi
-            JOIN `tabSales Order` so ON so.name = soi.parent
-            WHERE
-            soi.custom_box IN %(item_codes)s
-            AND so.transaction_date BETWEEN %(from_date)s AND %(to_date)s
-            AND so.docstatus = 1
-            GROUP BY soi.custom_box
-        """,
-            {
-                "item_codes": item_codes,
-                "from_date": self.filters["from_date"],
-                "to_date": self.filters["to_date"],
-            },
-            as_dict=1,
-        )
-        return so_data
+        rows = frappe.db.sql(f"""
+            SELECT ct.{item_field} AS item_code, SUM(ct.qty) AS total_qty
+            FROM `{child_table}` ct
+            JOIN `{doctype}` dt ON dt.name = ct.parent
+            WHERE ct.{item_field} IN %(item_codes)s
+            AND dt.{date_field} BETWEEN %(from_date)s AND %(to_date)s
+            AND dt.docstatus = 1
+            GROUP BY ct.{item_field}
+        """, {
+            "item_codes": item_codes,
+            "from_date": from_date,
+            "to_date": to_date,
+        }, as_dict=1)
+        
+        return {row.item_code: row.total_qty for row in rows}
 
     def build_data(self):
         items = self.fetch_items()
         if not items:
             return
-
-        item_codes = [item.item_code for item in items]
-
-        bin_data = self.fetch_bin_data(item_codes)
-        dn_data = self.fetch_dn_quantities(item_codes)
-        pr_data = self.fetch_pr_quantities(item_codes)
-        so_data = self.fetch_so_quantities(item_codes)
-
-        dn_map = {row.item_code: row.dn_qty for row in dn_data}
-        pr_map = {row.item_code: row.pr_qty for row in pr_data}
-        so_map = {row.item_code: row.so_qty for row in so_data}
-
-        bin_map = {}
-        for bin in bin_data:
-            bin_map[bin.item_code] = bin.actual_qty
         
-        # Build final rows
+        item_codes = [item['item_code'] for item in items]
+        bin_map = self.fetch_bin_data(item_codes)
+        range_data = {}
+
+        for i in self.get_range_indices():
+            from_date = self.filters.get(f"from_date_{i}")
+            to_date = self.filters.get(f"to_date_{i}")
+
+            if from_date and to_date:
+                dn_map = self.fetch_qty_map(item_codes, "tabDelivery Note", "tabDelivery Note Item", "item_code", "posting_date", from_date, to_date)
+                pr_map = self.fetch_qty_map(item_codes, "tabPurchase Receipt", "tabPurchase Receipt Item", "item_code", "posting_date", from_date, to_date)
+                so_map = self.fetch_qty_map(item_codes, "tabSales Order", "tabSales Order Item", "custom_box", "transaction_date", from_date, to_date)
+                range_data[i] = {"dn": dn_map, "pr": pr_map, "so": so_map}
+
         for item in items:
-            self.data.append(
-                {
-                    "item_code": item.item_code,
-                    "item_name": item.item_name,
-                    "stock_qty": bin_map.get(item.item_code, 0),
-                    "dn_qty": dn_map.get(item.item_code, 0),
-                    "pr_qty": pr_map.get(item.item_code, 0),
-                    "so_qty": so_map.get(item.item_code, 0),
-                }
-            )
+            row = {
+                "item_code": item['item_code'],
+                "item_name": item['item_name'],
+                "stock_qty": bin_map.get(item['item_code'], 0),
+            }
+
+            for i in self.get_range_indices():
+                row[f"dn_qty_{i}"] = range_data.get(i, {}).get("dn", {}).get(item['item_code'], 0)
+                row[f"pr_qty_{i}"] = range_data.get(i, {}).get("pr", {}).get(item['item_code'], 0)
+                row[f"so_qty_{i}"] = range_data.get(i, {}).get("so", {}).get(item['item_code'], 0)
+
+            self.data.append(row)
 
     def run(self):
         self.build_data()
