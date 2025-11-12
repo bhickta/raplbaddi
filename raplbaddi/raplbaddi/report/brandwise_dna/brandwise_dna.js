6b0abf85cac0a7c6ea2e2cf95bb944fed94ad591
Deepika <devrapl-two@gmail.com>
2025-11-12 13:54:00 +0530

    Fix: Updated Brandwise_DNA report — corrected freight logic, added item_code & status columns

--- raplbaddi/raplbaddi/report/brandwise_dna/brandwise_dna.js
+++ raplbaddi/raplbaddi/report/brandwise_dna/brandwise_dna.js
@@ -1,26 +1,43 @@
-// Copyright (c) 2025, Nishant Bhickta and contributors
-// For license information, please see license.txt
-
-
-frappe.query_reports["Brandwise DNA"] = {
-    "filters": [
-        {
-            "fieldname": "brand",
-            "label": __("Brand"),
-            "fieldtype": "Link",
-            "options": "Brand"
-        },
-        {
-            "fieldname": "customer",
-            "label": __("Customer"),
-            "fieldtype": "Link",
-            "options": "Customer"
-        },
-        {
-            "fieldname": "item_code",
-            "label": __("Item Code"),
-            "fieldtype": "Link",
-            "options": "Item"
-        }
-    ]
+frappe.query_reports["Brandwise_DNA"] = {
+  "filters": [
+    {
+      fieldname: "from_date",
+      label: "From Date",
+      fieldtype: "Date",
+      reqd: 1,
+      default: frappe.datetime.add_days(frappe.datetime.get_today(), -30)
+    },
+    {
+      fieldname: "to_date",
+      label: "To Date",
+      fieldtype: "Date",
+      reqd: 1,
+      default: frappe.datetime.get_today()
+    },
+    {
+      fieldname: "item_group",
+      label: "Item Group",
+      fieldtype: "Link",
+      options: "Item Group"
+    },
+    {
+      fieldname: "brand",
+      label: "Brand",
+      fieldtype: "Link",
+      options: "Brand"
+    },
+    {
+      fieldname: "customer",
+      label: "Customer",
+      fieldtype: "Link",
+      options: "Customer"
+    },
+    {
+      fieldname: "status",
+      label: "Status",
+      fieldtype: "Select",
+      options: ["", "Submitted", "Cancelled"],
+      default: ""
+    }
+  ]
 };
