7c863e53bdad6ea13958a2709b87ae3ee1beab3c
bhickta <61617746+bhickta@users.noreply.github.com>
2025-11-25 15:29:03 +0000

    feat: conditionally initialize git repository and commit deployed artifacts within the deploy script.

--- scripts/deploy_router.sh
+++ scripts/deploy_router.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
 set -e
 
-# Configuration
 APP="raplbaddi"
 ARTIFACTS="/home/frappe/codedeploy-artifacts/raplbaddi"
 
-# Determine Bench Path
 if [ "$DEPLOYMENT_GROUP_NAME" == "Rapl-Prod-Group" ]; then
     BENCH_DIR="/home/frappe/prod-bench"
 else
     BENCH_DIR="/home/frappe/dev-bench"
 fi
 
-# 1. Root Steps: Update Code & Permissions
 echo "Updating code in $BENCH_DIR/apps/$APP..."
 rm -rf "$BENCH_DIR/apps/$APP"
 cp -r "$ARTIFACTS" "$BENCH_DIR/apps/$APP"
 chown -R frappe:frappe "$BENCH_DIR/apps/$APP"
 
-# 2. Frappe Steps: Bench Commands
-# We use a Heredoc (<<EOF) to run this block as the 'frappe' user
 sudo -i -u frappe bash <<EOF
 set -e
 export NVM_DIR="\$HOME/.nvm"
 [ -s "\$NVM_DIR/nvm.sh" ] && \. "\$NVM_DIR/nvm.sh"
+
+cd $BENCH_DIR/apps/$APP
+
+if [ ! -d ".git" ]; then
+    git init
+    git config user.email "deploy@bot.com"
+    git config user.name "DeployBot"
+    git add .
+    git commit -m "Deploy artifact"
+fi
 
 cd $BENCH_DIR
 
